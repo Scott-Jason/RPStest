8c4a95454a7ddaa75a1ae4131a0640d1af4f2c9a
Jason Scott <jcpoems@gmail.com>
2024-06-18 15:59:34 -0400

    Create a UI for rock paper scissors
    I went with a pixel style and grabbed a font off google and made sure the colors were sorta cohesive.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,38 @@
+let humanScore = 0;
+let computerScore = 0;
 
+const score = document.querySelector("h2");
+const options = document.querySelector(".options");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const numScore = document.querySelector("h3");
+
+options.addEventListener('click', (event) => {
+    let target = event.target;
+    switch(target.id) {
+        case 'rock':
+            playRound("rock", getComputerChoice());
+            break;
+        case 'paper':
+            playRound("paper", getComputerChoice());
+            break;
+        case 'scissors':
+            playRound("scissors", getComputerChoice());
+            break;
+    }
+    if(humanScore == 5){
+        score.textContent = "YOU WON 5 FIRST, ENJOY YOUR RANDOM SUCCESS!"
+        humanScore = 0;
+        computerScore = 0;
+    }
+    if(computerScore == 5){
+        score.textContent = "You've been beaten by a robot. You lack worth."
+        humanScore = 0;
+        computerScore = 0;
+    }
+    numScore.textContent = "Human " + humanScore + " Robot: " + computerScore;
+});
 
 function getComputerChoice(){
     let rand = Math.random();
@@ -11,42 +45,35 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    return prompt("Rock, Paper, or Scissors?").toLowerCase();
-}
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(human,cpu){
-        if(human == cpu){
-            console.log("You both threw " + human + "; Strange.");
-            return;
-        }
-        if(human == "rock" && cpu != "paper"){
-            humanScore++;
-            console.log("You WON! " + human + " beats " + cpu );
-            return;
-        }
-        if(human == "paper" && cpu != "scissors"){
-            humanScore++;
-            console.log("You WON! " + human + " beats " + cpu );
-            return;
-        }
-        if(human == "scissors" && cpu != "rock"){
-            humanScore++;
-            console.log("You WON! " + human + " beats " + cpu );
-            return;
-        }
-        computerScore++;
-        console.log("You LOST! " + cpu + " beats " + human );
+function playRound(human,cpu){
+    if(human == cpu){
+        score.textContent = "You both threw " + human + "; Strange.";
+        console.log("You both threw " + human + "; Strange.");
         return;
     }
-    for(let i = 0; i < 5; i++){
-        playRound(getHumanChoice(), getComputerChoice());
+    if(human == "rock" && cpu != "paper"){
+        humanScore++;
+        score.textContent = "You WON! " + human + " beats " + cpu;
+        console.log("You WON! " + human + " beats " + cpu );
+        
+        return;
     }
-    console.log("The final score was: YOU: " + humanScore + " CPU: " + computerScore);
-
-
+    if(human == "paper" && cpu != "scissors"){
+        humanScore++;
+        score.textContent = "You WON! " + human + " beats " + cpu;
+        console.log("You WON! " + human + " beats " + cpu );
+        return;
+    }
+    if(human == "scissors" && cpu != "rock"){
+        humanScore++;
+        score.textContent = "You WON! " + human + " beats " + cpu;
+        console.log("You WON! " + human + " beats " + cpu );
+        return;
+    }
+    computerScore++;
+    score.textContent = "You LOST! " + cpu + " beats " + human;
+    console.log("You LOST! " + cpu + " beats " + human );
+    return;
 }
-
-playGame();
+   
+    
